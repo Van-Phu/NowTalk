2216b026f72db5568c9adbd5def844986c9c4cab
Van Phu <luongphu08@gmail.com>
2024-01-03 15:33:03 +0700

    Update Login

--- nowtalk/src/screen/App.js
+++ nowtalk/src/screen/App.js
@@ -2,43 +2,50 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://curly-pollen-steam.glitch.me');
 
 const App = () => {
   const [messages, setMessages] = useState([]);
   const [user, setUser] = useState('');
   const [content, setContent] = useState('');
 
+  
+
   useEffect(() => {
-    // Fetch initial messages from the server when the component mounts
     axios.get('https://curly-pollen-steam.glitch.me/api/messages/getMessages')
       .then(response => setMessages(response.data))
       .catch(error => console.error(error));
+  
     socket.on('chat message', newMessage => {
       setMessages(prevMessages => [...prevMessages, newMessage]);
     });
+  
     return () => {
       socket.off('chat message');
     };
   }, []);
+  
 
   const sendMessage = () => {
     if (user.trim() === '' || content.trim() === '') {
       return;
     }
-
+  
     const newMessage = { user, content };
-    setMessages(prevMessages => [...prevMessages, newMessage]);
+  
     axios.post('https://curly-pollen-steam.glitch.me/api/messages/postMessages', newMessage)
       .then(response => {
         if (response.data.success) {
           setMessages(prevMessages => [...prevMessages, newMessage]);
           socket.emit('chat message', newMessage);
           setContent('');
+        } else {
+          console.error('Failed to send message:', response.data.error);
         }
       })
       .catch(error => console.error(error));
   };
+  
 
   return (
     <div>
---
+++ nowtalk/src/screen/login.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import '../css/login.css'
+import { Icon } from '@iconify/react';
+import axios from 'axios';
+
+
+const Login = () =>{
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleGetValueUsername = (event) => {
+        const value = event.target.value;
+        setUsername(value);
+      };
+
+    const handleGetValuePassword = (event) => {
+        const value = event.target.value;
+        setPassword(value);
+    };
+
+    const handleLogin = () => {
+        if (!username.trim() || !password.trim()) {
+          alert("Vui lòng nhập tên đăng nhập và mật khẩu.");
+          return;
+        }
+        axios.post('https://curly-pollen-steam.glitch.me/api/users/login', {
+          username: username,
+          password: password,
+        })
+        .then(response => {
+          console.log("Đăng nhập thành công:", response.data);
+          alert('Đăng nhập thành công:!');
+        })
+        .catch(error => {
+          console.error("Đăng nhập thất bại:", error);
+        });
+      };
+
+    return(
+        <div className='safeArea'>
+            <div className='imageBackground'></div>
+            <div className='formLogin'>
+                <h1 style={{color:'white', fontSize: 40}}>ĐĂNG NHẬP</h1>
+
+                <div className='input'>
+                    <p className='textInput'>Tài khoản</p>
+                    <div className='inputType'>
+                        <Icon icon="solar:user-broken" color="#a4a4a4" fontSize={30} style={{margin: 10}} />
+                        <input value={username} onChange={handleGetValueUsername} style={{height: 30, width: '100%', marginRight: 10, background: "#190733", border: 0, color: "#A4A4A4"}}></input>
+                    </div>
+                </div>
+
+                <div className='input'>
+                    <p className='textInput'>Mật khẩu</p>
+                    <div className='inputType'>
+                        <Icon icon="mdi:password-outline" color="#a4a4a4" fontSize={30} style={{margin: 10}} />
+                        <input value={password} onChange={handleGetValuePassword} style={{height: 30, width: '100%', marginRight: 10, background: "#190733", border: 0, color: "#A4A4A4"}}></input>
+                    </div>
+                </div>
+
+                <button onClick={handleLogin} className='btnLogin'>Đăng nhập</button>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
